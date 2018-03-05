10d44d86719b07126682ae2c930691a428544d35
José M. Pérez <JMPerez@users.noreply.github.com>
2018-03-05 08:47:12 +0100

    Avoid passing device_id in body parameters (#190)

--- src/spotify-web-api.js
+++ src/spotify-web-api.js
@@ -1083,12 +1083,20 @@ SpotifyWebApi.prototype = {
    *          otherwise an error. Not returned if a callback is given.
    */
   play: function(options, callback) {
+    /*jshint camelcase: false */
+    var _options = options || {};
+    var queryParams = _options.device_id ? {device_id: _options.device_id} : null;
+    var postData = {};
+    ['context_uri', 'uris', 'offset'].forEach(function(field) {
+      if (field in _options) {
+        postData[field] = _options[field];
+      }
+    });
     return WebApiRequest.builder(this.getAccessToken())
       .withPath('/v1/me/player/play')
-      /*jshint camelcase: false */
-      .withQueryParameters(options && options.device_id ? {device_id: options.device_id} : null)
+      .withQueryParameters(queryParams)
       .withHeaders({ 'Content-Type' : 'application/json' })
-      .withBodyParameters(options)
+      .withBodyParameters(postData)
       .build()
       .execute(HttpManager.put, callback);
   },
