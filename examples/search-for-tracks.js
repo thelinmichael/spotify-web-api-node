703f1a154517bb05ff3b4a6f51e5f6619f4c0b36
dandv <ddascalescu+github@gmail.com>
2018-09-19 00:45:16 -0700

    Fix Search example (requires authorization)

--- examples/search-for-tracks.js
+++ examples/search-for-tracks.js
@@ -1,35 +1,58 @@
-var SpotifyWebApi = require('../');
+const SpotifyWebApi = require('../');
 
 /*
  * This example shows how to search for a track. The endpoint is documented here:
- * https://developer.spotify.com/web-api/search-item/
+ * https://developer.spotify.com/documentation/web-api/reference/search/
 
- * Please note that this endpoint does not require authentication. However, using an access token
- * when making requests will give your application a higher rate limit.
+ * Since authorization is now required, this example retrieves an access token using the Authorization Code Grant flow,
+ * documented here: https://developer.spotify.com/documentation/general/guides/authorization-guide/#authorization-code-flow
+ *
+ * Obtain the `authorizationCode` below as described in the Authorization section of the README.
  */
 
-var spotifyApi = new SpotifyWebApi();
+const authorizationCode = '<insert authorization code>';
 
-spotifyApi.searchTracks('Love', function(err, data) {
-  if (err) {
-    console.error('Something went wrong', err.message);
-    return;
-  }
-
-  // Print some information about the results
-  console.log('I got ' + data.body.tracks.total + ' results!');
-
-  // Go through the first page of results
-  var firstPage = data.body.tracks.items;
-  console.log('The tracks in the first page are.. (popularity in parentheses)');
-
-  /*
-   * 0: All of Me (97)
-   * 1: My Love (91)
-   * 2: I Love This Life (78)
-   * ...
-   */
-  firstPage.forEach(function(track, index) {
-    console.log(index + ': ' + track.name + ' (' + track.popularity + ')');
-  });
+/**
+ * Get the credentials from Spotify's Dashboard page.
+ * https://developer.spotify.com/dashboard/applications
+ */
+const spotifyApi = new SpotifyWebApi({
+  clientId: '<insert client id>',
+  clientSecret: '<insert client secret>',
+  redirectUri: '<insert redirect URI>'
 });
+
+spotifyApi
+  .authorizationCodeGrant(authorizationCode)
+  .then(function(data) {
+    console.log('Retrieved access token', data.body['access_token']);
+
+    // Set the access token
+    spotifyApi.setAccessToken(data.body['access_token']);
+
+    // Use the access token to retrieve information about the user connected to it
+    return spotifyApi.searchTracks('Love');
+  })
+  .then(function(data) {
+    // Print some information about the results
+    console.log('I got ' + data.body.tracks.total + ' results!');
+
+    // Go through the first page of results
+    var firstPage = data.body.tracks.items;
+    console.log(
+      'The tracks in the first page are.. (popularity in parentheses)'
+    );
+
+    /*
+     * 0: All of Me (97)
+     * 1: My Love (91)
+     * 2: I Love This Life (78)
+     * ...
+     */
+    firstPage.forEach(function(track, index) {
+      console.log(index + ': ' + track.name + ' (' + track.popularity + ')');
+    });
+  })
+  .catch(function(err) {
+    console.log('Something went wrong:', err.message);
+  });
