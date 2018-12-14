c17b84e090c431b7ece4a54b297a7d6a8927427c
Caleb <chialeilee@umass.edu>
2018-12-14 17:02:46 -0500

    include auth in get-related-artists example #155

--- examples/get-related-artists.js
+++ examples/get-related-artists.js
@@ -4,16 +4,36 @@ var SpotifyWebApi = require('../');
  * This example shows how to get artists related to another artists. The endpoint is documented here:
  * https://developer.spotify.com/web-api/get-related-artists/
 
- * Please note that this endpoint does not require authentication. However, using an access token
- * when making requests will give your application a higher rate limit.
+ * Please note that authorization is now required and so this example retrieves an access token using the Authorization Code Flow,
+ * documented here: https://developer.spotify.com/documentation/general/guides/authorization-guide/#authorization-code-flow
  */
 
-var spotifyApi = new SpotifyWebApi();
+var authorizationCode =
+  'AQAgjS78s64u1axMCBCRA0cViW_ZDDU0pbgENJ_-WpZr3cEO7V5O-JELcEPU6pGLPp08SfO3dnHmu6XJikKqrU8LX9W6J11NyoaetrXtZFW-Y58UGeV69tuyybcNUS2u6eyup1EgzbTEx4LqrP_eCHsc9xHJ0JUzEhi7xcqzQG70roE4WKM_YrlDZO-e7GDRMqunS9RMoSwF_ov-gOMpvy9OMb7O58nZoc3LSEdEwoZPCLU4N4TTJ-IF6YsQRhQkEOJK';
+
+/* Set the credentials given on Spotify's My Applications page.
+ * https://developer.spotify.com/my-applications
+ */
+var spotifyApi = new SpotifyWebApi({
+  clientId: '<insert client id>',
+  clientSecret: '<insert client secret>',
+  redirectUri: '<insert redirect URI>'
+});
 
 var artistId = '0qeei9KQnptjwb8MgkqEoy';
 
-spotifyApi.getArtistRelatedArtists(artistId).then(
-  function(data) {
+spotifyApi
+.authorizationCodeGrant(authorizationCode)
+  .then(function(data) {
+    console.log('Retrieved access token', data.body['access_token']);
+
+    // Set the access token
+    spotifyApi.setAccessToken(data.body['access_token']);
+
+    // Use the access token to retrieve information about the user connected to it
+    return spotifyApi.getArtistRelatedArtists(artistId);
+  })
+  .then(function(data) {
     if (data.body.artists.length) {
       // Print the number of similar artists
       console.log('I got ' + data.body.artists.length + ' similar artists!');
