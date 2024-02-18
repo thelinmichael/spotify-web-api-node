dcf7d80b1a092cea63bff75e4eca2ee567330694
Andriy Bohdan <a.bohdan@gmail.com>
2024-02-18 20:44:01 +0200

    If SpotifyWebApiInternalConfig global variable is set, use it as the config for Internal API

--- src/base-request.js
+++ src/base-request.js
@@ -7,6 +7,7 @@ var Request = function(builder) {
 
   this.host = builder.host;
   this.port = builder.port;
+  this.pathPrefix = builder.pathPrefix;
   this.scheme = builder.scheme;
   this.queryParameters = builder.queryParameters;
   this.bodyParameters = builder.bodyParameters;
@@ -23,6 +24,8 @@ Request.prototype._getter = function(key) {
 Request.prototype.getHost = Request.prototype._getter('host');
 
 Request.prototype.getPort = Request.prototype._getter('port');
+
+Request.prototype.getPathPrefix = Request.prototype._getter('pathPrefix');
 
 Request.prototype.getScheme = Request.prototype._getter('scheme');
 
@@ -48,6 +51,9 @@ Request.prototype.getURI = function() {
     (this.scheme === 'https' && this.port !== 443)
   ) {
     uri += ':' + this.port;
+  }
+  if (this.pathPrefix) {
+    uri += this.pathPrefix;
   }
   if (this.path) {
     uri += this.path;
@@ -112,16 +118,45 @@ Builder.prototype.withHost = Builder.prototype._setter('host');
 
 Builder.prototype.withPort = Builder.prototype._setter('port');
 
+Builder.prototype.withPathPrefix = Builder.prototype._setter('pathPrefix');
+
 Builder.prototype.withScheme = Builder.prototype._setter('scheme');
 
 Builder.prototype.withPath = Builder.prototype._setter('path');
+
+Builder.prototype.withInternalApi = function(config) {
+  var that = this;
+  if (typeof(config) === 'object') {
+    var url = new URL(config.apiBase);
+    var host = url.hostname;
+    var port = url.port;
+    var scheme = url.protocol.replace(':', '');
+
+    var that = that.withHost(host);
+    that = that.withPort(port);
+    that = that.withScheme(scheme);
+    that = that.withPathPrefix(config.apiPathPrefix);
+    that = that.withHeaders({ "x-api-key": config.apiKey });
+
+    if (typeof config.waitFor == 'number')  {
+      that = that.withHeaders({ "x-wait-for": `${config.waitFor}` })
+    }
+    if (config.noCache == true)  {
+      that = that.withHeaders({ "x-no-cache": "true" })
+    }
+    if (config.noQueue == true)  {
+      that = that.withHeaders({ "x-no-queue": "true" })
+    }
+  }
+  return that;
+};
 
 Builder.prototype._assigner = function(key) {
   return function() {
     for (var i = 0; i < arguments.length; i++) {
       this[key] = this._assign(this[key], arguments[i]);
     }
-    
+
     return this;
   };
 };
--- src/webapi-request.js
+++ src/webapi-request.js
@@ -8,9 +8,9 @@ var DEFAULT_HOST = 'api.spotify.com',
 
 module.exports.builder = function(accessToken) {
   return Request.builder()
-    .withHost(globalThis.INTERNAL_API_HOST===undefined ? DEFAULT_HOST : INTERNAL_API_HOST)
-    .withPort(globalThis.INTERNAL_API_PORT===undefined ? DEFAULT_PORT : INTERNAL_API_PORT)
-    .withScheme(globalThis.INTERNAL_API_SCHEME===undefined ? DEFAULT_SCHEME : INTERNAL_API_SCHEME)
-    .withHeaders({ "x-api-key": globalThis.INTERNAL_API_API_KEY })
-    .withAuth(accessToken);
+    .withHost(DEFAULT_HOST)
+    .withPort(DEFAULT_PORT)
+    .withScheme(DEFAULT_SCHEME)
+    .withAuth(accessToken)
+    .withInternalApi(globalThis.SpotifyWebApiInternalConfig);
 };
